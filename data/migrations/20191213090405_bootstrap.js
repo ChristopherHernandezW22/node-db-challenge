e301443be2c6f31455bdbcc44cc0c56db9d0f862
Christopher Hernandez <cerberean661@gmail.com>
2019-12-13 09:25:31 -0800

    filling out migrations table lists

--- data/migrations/20191213090405_bootstrap.js
+++ data/migrations/20191213090405_bootstrap.js
@@ -1,8 +1,23 @@
 
 exports.up = function(knex) {
-  return knex.schema.createTable('', tale => {
-      table.increments()
-  });
+  return knex.schema
+    .createTable('projects', table => {
+        table.increments();
+        table.string('name').notNullable();
+        table.text('description');
+        table.boolean('completed').notNullable().defaultTo(false);
+  })
+    .createTable('resources', table => {
+        table.increments();
+        table.string('name').notNullable();
+        table.text('description')
+  })
+    .creatTable('tasks', table => {
+        table.increments();
+        table.text('description').notNullable();
+        table.text('notes');
+        table.boolean('completed').notNullable().defaultTo(false);
+    })
 };
 
 exports.down = function(knex) {
